2082a25c7a3a86106afffa53a9da6bdf868f6c1a
Henry <hnr.leto11@gmail.com>
2020-11-12 12:25:35 -0500

    revert friend / friendship relationship

--- app/models/friend_request.rb
+++
@@ -1,4 +0,0 @@
-class FriendRequest < ApplicationRecord
-  belongs_to :requestor, class_name: 'user'
-  belongs_to :receiver, class_name: 'user'
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
   has_many :posts
-
-  has_many :friend_requests_as_requestor, foreign_key: 'requestor_id', class_name: 'friend_request'
-  has_many :friend_requests_as_receiver, foreign_key: 'receiver_id', class_name: 'friend_request'
 end
