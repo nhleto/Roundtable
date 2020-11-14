47cc25dcd629666e4e42aa0de2d4566f6b3319fc
Henry <hnr.leto11@gmail.com>
2020-11-14 18:50:34 -0500

    add gravatar gem and format rudimentary feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @user = User.find(current_user[:id])
   end
 
   # GET /posts/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+
+User.create!(name: 'henry', username: 'username', password: 'password', email: 'me@me.com', date_of_birth: '2020-01-01')
+
+10.times do
+  User.create!(name: Faker::FunnyName.unique.name, password: 'password', username: Faker::GreekPhilosophers.name,
+               email: Faker::Internet.unique.email, date_of_birth: Faker::Date.between(from: '1997-01-01', to: '1999-01-01'))
+end
+
+20.times do
+  Post.create!(title: Faker::Book.unique.title, body: Faker::Lorem.unique.paragraphs, user_id: User.ids.sample)
+end
