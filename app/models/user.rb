dd930f0556054e9ddb1564799e9a020deed133b0
Henry <hnr.leto11@gmail.com>
2020-11-21 11:25:59 -0500

    added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
