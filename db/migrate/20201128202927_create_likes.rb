ba7de3064c4ecd294ad1bcc6d245ea9bcb92cd10
Henry <hnr.leto11@gmail.com>
2020-11-28 15:30:02 -0500

    add likes model

---
+++ db/migrate/20201128202927_create_likes.rb
@@ -0,0 +1,12 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.string :name
+      t.integer :likeable_id
+      t.string :likeable_type
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
