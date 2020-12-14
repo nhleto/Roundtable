a4f76a1bb5733d51220877511f0849d2de8b0d23
Henry <hnr.leto11@gmail.com>
2020-12-14 17:44:04 -0500

    add post photo imbed and styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -110,6 +110,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:title, :body, :user_id, :group_id)
+    params.require(:post).permit(:title, :body, :user_id, :group_id, images: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  has_many_attached :images
+  validates :body, presence: true
+
   belongs_to :user
   belongs_to :group, optional: true
   has_many :comments, dependent: :destroy
