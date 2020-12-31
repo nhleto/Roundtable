c1e6bddf77b06dff9a5b2e86b48f7fcec89595be
Henry <hnr.leto11@gmail.com>
2020-12-31 13:50:59 -0500

    created model methods for image variants

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # def mutual_friends(users, current_user)
-  #   (users - current_user.friends).reject { |user| user == current_user }
-  # end
+  def banner
+    group_photo.variant(resize: '730x500!')
+  end
+
+  def thumbnail
+    group_photo.variant(resize: '223x150!')
+  end
+
+  def square
+    images.variant(resize: '250x250')
+  end
 end
